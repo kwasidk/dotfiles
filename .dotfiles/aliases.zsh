71a7e6f1a29155196e523c8f24459570ace4e82e
Kwasi Kattah <kwasi@kattah.nl>
2022-05-11 15:05:09 +0200

    Dotfiles: Initial Commit

---
+++ .dotfiles/aliases.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# git aliases
+alias status="git status"
+alias switch="git switch"
+alias commit="git add . && git commit -m"
+alias push="git push"
+alias pull="git pull"
+alias clone="git clone"
+
+# laravel
+alias sail="vendor/bin/sail"
+alias art="php artisan"
